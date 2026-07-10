1987e2cfa5353f2e8cb85580a9406d54ccf38df7
w0jno <filip.wojno03@gmail.com>
2026-07-10 16:38:14 +0200

    init cv-test-01 machine using cloudinit

--- terraform/environments/production/main.tf
+++ terraform/environments/production/main.tf
@@ -1,10 +1,27 @@
-resource "proxmox_cloned_vm" "production" {
+resource "proxmox_virtual_environment_vm" "production" {
   node_name = "pve"
   name      = "cv-test-01"
-  id     = 9001
-
-  clone = {
-    source_vm_id = 9000
-    full         = true
+  vm_id     = 9001
+  stop_on_destroy = true
+  clone {
+    vm_id = 9000
+    full  = true
   }
+
+  network_device {
+    bridge = "vmbr0"
+  } 
+  initialization {
+    user_account {
+      username = "ubuntu"
+      keys = [var.ssh_ubuntu_key]
+    }
+    ip_config {
+      ipv4 {
+        address = "192.168.1.201/24"
+        gateway = "192.168.1.1"
+      }
+    }
+  }
+
 }
--- terraform/environments/production/variable.tf
+++ terraform/environments/production/variable.tf
@@ -7,3 +7,8 @@ variable "proxmox_api_token" {
   type        = string
   description = "The API token for the Proxmox VE API"
 }
+
+variable "ssh_ubuntu_key" {
+  type        = string
+  description = "The SSH key for the Ubuntu user"
+}
